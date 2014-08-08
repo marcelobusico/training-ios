a8e94467a2c5562ba87f65cd32d5fba6d7a962e7
Marcelo Busico <marcelo.busico@mercadolibre.com>
2014-08-08 12:57:02 -0300

    Using auto layout.

---
+++ RayWenderlichTutorial/UsingAutolayout/StrutsProblem/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  StrutsProblem
+//
+//  Created by Marcelo Busico on 08/08/14.
+//  Copyright (c) 2014 Marcelo Busico. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *topLeftView;
+@property (weak, nonatomic) IBOutlet UIView *topRightView;
+@property (weak, nonatomic) IBOutlet UIView *bottomView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
