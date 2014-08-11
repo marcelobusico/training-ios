fecb40da23c2b3ae20e3c1623f0f4feca5dc3d32
Marcelo Busico <marcelo.busico@mercadolibre.com>
2014-08-11 17:43:53 -0300

    Base project.

---
+++ Ejercicio4/MyItems/MyItems/MNBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MNBAppDelegate.h
+//  MyItems
+//
+//  Created by Marcelo Busico on 28/07/14.
+//  Copyright (c) 2014 Marcelo Busico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
