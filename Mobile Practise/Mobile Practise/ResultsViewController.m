431d982b80ce16557f0d5ebe617e0f3c87e2182d
Marcelo Busico <marcelo.busico@mercadolibre.com>
2014-08-19 18:56:44 -0300

    Using UIImageView category from AFNetworking to download images.

--- Mobile Practise/Mobile Practise/ResultsViewController.m
+++ Mobile Practise/Mobile Practise/ResultsViewController.m
@@ -9,92 +9,76 @@
 #import "ResultsViewController.h"
 #import "ItemCell.h"
 #import "ItemEntity.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface ResultsViewController ()
 
-@property(nonatomic,strong) IBOutlet UITableView *tblResults;
+@property (nonatomic, strong) IBOutlet UITableView *tblResults;
 
 @end
 
 @implementation ResultsViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+	if (self) {
+		// Custom initialization
+	}
+	return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
+- (void)viewDidLoad {
+	[super viewDidLoad];
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [_results count];
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return [_results count];
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"itemCell";
-    
-    ItemCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    
-    if(cell == nil) {
-        [tableView registerNib:[UINib nibWithNibName:@"ItemCell" bundle:nil]
-        forCellReuseIdentifier:CellIdentifier];
-        
-        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    }
-    
-    return cell;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	static NSString *CellIdentifier = @"itemCell";
+
+	ItemCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+
+	if (cell == nil) {
+		[tableView registerNib:[UINib nibWithNibName:@"ItemCell" bundle:nil]
+		           forCellReuseIdentifier:CellIdentifier];
+
+		cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	}
+
+	return cell;
 }
 
--(void)tableView:(UITableView *)tableView willDisplayCell:(ItemCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView willDisplayCell:(ItemCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+	ItemEntity *itemEntity = [_results objectAtIndex:indexPath.row];
 
-    ItemEntity *itemEntity = [_results objectAtIndex:indexPath.row];
+	NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+	NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"es_AR"];
+	UIImage *placeholder = [UIImage imageNamed:@"placeholderIcon"];
 
-    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
-    NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"es_AR"];
-    
-    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
-    [formatter setLocale:locale];
-    
-    cell.lblTitle.text = itemEntity.title;
-    cell.lblPrice.text = [formatter stringFromNumber:itemEntity.price];
-    cell.itemImage.image = [UIImage imageNamed:@"placeholderIcon"];
-    
-    if(itemEntity.imageURL) {
-        NSURL *imageUrl = [NSURL URLWithString:itemEntity.imageURL];
-        
-        [self downloadImageWithURL:imageUrl completionBlock:^(BOOL succeeded, UIImage *image) {
-            if(succeeded) {
-                cell.itemImage.image = image;
-            }
-        }];
-    }
+	[formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+	[formatter setLocale:locale];
+
+	cell.lblTitle.text = itemEntity.title;
+	cell.lblPrice.text = [formatter stringFromNumber:itemEntity.price];
+	cell.itemImage.image = [UIImage imageNamed:@"placeholderIcon"];
+	__weak ItemCell *weakCell = cell;
+
+	if (itemEntity.imageURL) {
+		NSURL *url = [NSURL URLWithString:itemEntity.imageURL];
+		NSURLRequest *request = [NSURLRequest requestWithURL:url];
+		[cell.itemImage setImageWithURLRequest:request
+		                      placeholderImage:placeholder
+		                               success: ^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+		    weakCell.itemImage.image = image;
+		    [weakCell setNeedsLayout];
+		} failure:nil];
+	}
 }
 
-- (void)downloadImageWithURL:(NSURL *)url completionBlock:(void (^)(BOOL succeeded, UIImage *image))completionBlock
-{
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request
-                                       queue:[NSOperationQueue mainQueue]
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-                               if ( !error )
-                               {
-                                   UIImage *image = [[UIImage alloc] initWithData:data];
-                                   completionBlock(YES,image);
-                               } else{
-                                   completionBlock(NO,nil);
-                               }
-                           }];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
 }
 
 @end
